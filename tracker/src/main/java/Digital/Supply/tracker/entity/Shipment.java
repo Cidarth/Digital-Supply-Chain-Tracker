4cb631655eec276bca48ce5499a97a12ee1f2ca2
Wickeshhh <psakhilesh2003@gmail.com>
2025-05-29 23:28:02 +0530

    update CheckpointLog

---
+++ tracker/src/main/java/Digital/Supply/tracker/entity/Shipment.java
@@ -0,0 +1,34 @@
+package Digital.Supply.tracker.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Shipment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id", nullable = false)
+    private Item item;
+
+    private String fromLocation;
+    private String toLocation;
+    private String expectedDelivery;
+
+    @Enumerated(EnumType.STRING)
+    private Status currentStatus;
+
+    @ManyToOne
+    @JoinColumn(name = "assigned_transporter_id", nullable = true)
+    private User assignedTransporter; // This field is nullable!
+
+    public enum Status {
+        CREATED, IN_TRANSIT, DELIVERED, PENDING, SHIPPED, DELAYED
+    }
+}
