7896f12a084215471f5e55dcd1fd8c5094c1322d
= <=>
2025-05-29 10:50:24 +0530

    Added item module and shipment Module

--- tracker/tracker - Copy/src/main/java/Digital/Supply/tracker/repository/AlertRepository.java
+++
@@ -1,6 +0,0 @@
-package Digital.Supply.tracker.repository;
-
-import Digital.Supply.tracker.entity.Alert;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlertRepository extends JpaRepository<Alert, Long> {}
--- tracker/tracker - Copy/src/main/java/Digital/Supply/tracker/repository/ItemRepository.java
+++
@@ -1,6 +0,0 @@
-package Digital.Supply.tracker.repository;
-
-import Digital.Supply.tracker.entity.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {}
