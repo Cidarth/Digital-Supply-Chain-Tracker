1ff48e5314b1b94fe3e725724c0d4261809efd17
rick-bren <kencodebren@gmail.com>
2025-05-29 15:37:14 +0530

    Delete tracker directory

--- tracker/src/main/java/Digital/Supply/tracker/repository/CheckpointLogRepository.java
+++
@@ -1,9 +0,0 @@
-package Digital.Supply.tracker.repository;
-
-import Digital.Supply.tracker.entity.CheckpointLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface CheckpointLogRepository extends JpaRepository<CheckpointLog, Long> {
-    List<CheckpointLog> findByShipmentId(Long shipmentId);
-}
