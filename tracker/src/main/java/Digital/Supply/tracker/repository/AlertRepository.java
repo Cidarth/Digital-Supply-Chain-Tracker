a6802df5bcf173d884087c001e39acc185d790c5
MOHANA SREENATH <103328357+MohanaSreenath@users.noreply.github.com>
2025-05-30 15:15:13 +0530

    Delete tracker/src/main/java/Digital/Supply/tracker directory

--- tracker/src/main/java/Digital/Supply/tracker/repository/AlertRepository.java
+++
@@ -1,8 +0,0 @@
-package Digital.Supply.tracker.repository;
-
-import Digital.Supply.tracker.entity.Alert;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlertRepository extends JpaRepository<Alert, Long> {
-    boolean existsByShipmentIdAndTypeAndResolvedFalse(Long shipmentId, String type);
-}
