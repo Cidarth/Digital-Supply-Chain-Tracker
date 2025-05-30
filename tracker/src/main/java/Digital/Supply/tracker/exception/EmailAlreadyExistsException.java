5e6de6dc6632de65b1136e6f0002b69d0dd4675f
MohanaSreenath <mohanasreenathk@gmail.com>
2025-05-30 16:13:40 +0530

    Alerts

---
+++ tracker/src/main/java/Digital/Supply/tracker/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package Digital.Supply.tracker.exception;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException(String message) {
+        super(message);
+    }
+}
